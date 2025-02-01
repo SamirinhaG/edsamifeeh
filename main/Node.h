cc69bec39fedb8cccaf277f75d19127e00659d13
SamirinhaG <samiragomes@alu.ufc.br>
2025-02-01 18:07:39 -0300

    Update Node.h

--- main/Node.h
+++ main/Node.h
@@ -1,19 +1,21 @@
 #ifndef NODE_H
 #define NODE_H
 
-struct Node
-{
-  Node *direita;
-  Node *abaixo;
-  int linha;
-  int coluna;
+// Node.h define a estrutura do nó da matriz esparsa
+
+struct Node {
+
+  Node *direita; // Próximo elemento na mesma linha
+  Node *abaixo; // Próximo elemento na mesma coluna
+  int linha, coluna; 
   double valor;
 
+  // Construtor:
   Node(int linha, int coluna, double valor)
     : direita{this}, abaixo{this}, linha{linha}, coluna{coluna}, valor{valor}
+    // Direita e abaixo recebem eles mesmos(this), apontando para o próprio nó (indicando que, inicialmente, forma-se uma lista circular para eles mesmo)
   {}
 };
 
 #endif
 
-//direita e abaixo recebem ele mesmos, apontando para o próprio nó (indicando que, inicialmente, forma-se uma uma lista circular para ele mesmo)
